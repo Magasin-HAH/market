2fa7d0ab515ab4b3861e39dee3f0b65e03952248
Hamza-BenJabeur <hamza.mhbj@gmail.com>
2020-11-30 19:19:39 +0100

    work

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shop.component.css']
 })
 export class ShopComponent implements OnInit {
-
+isDisplay1=false;
+isDisplay2=false;
+isDisplay3=false;
   constructor() { }
 
   ngOnInit(): void {
   }
+toggleDisplay1(){
+  this.isDisplay1=true;
+  this.isDisplay2=false;
+this.isDisplay3=false;
 
+}
+toggleDisplay2(){
+  this.isDisplay1=false;
+  this.isDisplay2=true;
+this.isDisplay3=false;
+
+}
+toggleDisplay3(){
+  this.isDisplay1=false;
+  this.isDisplay2=false;
+this.isDisplay3=true;
+
+}
 }
